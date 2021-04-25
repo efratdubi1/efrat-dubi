c26da6599c6a1a610cbc40591f32f5de38a3f699
efrat-dubi <efratdubi@live.com>
2021-04-25 09:58:46 +0300

    EX6

---
+++ CV/cv.js
@@ -0,0 +1,11 @@
+let txt="Welcome To My CV Webpage";
+let i=0;
+let speed= 70;
+
+function Typewriter(){
+    if(i< txt.length){
+        document.getElementById("welcome").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(Typewriter,speed);
+    }
+}
